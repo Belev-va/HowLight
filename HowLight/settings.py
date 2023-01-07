26a001e7ded4840fdc1a56b1f6cde2daf14bdf05
reganztv <reganztv@gmail.com>
2023-01-07 18:36:56 +0300

    settings upd
    Signed-off-by: reganztv <reganztv@gmail.com>

--- HowLight/settings.py
+++ HowLight/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+dotenv_path = os.path.join('.env.dev')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
