59ef20e9797272d6b98cffa1ef71076da155c337
Belev-va <belev.job@gmail.com>
2023-10-29 02:01:06 +0400

    update url

--- HowLight/settings.py
+++ HowLight/settings.py
@@ -91,8 +91,8 @@ DATABASES = {
         'NAME': os.environ.get("SQL_DATABASE"),
         'USER': os.environ.get("SQL_USER"),
         'PASSWORD': os.environ.get("SQL_PASSWORD"),
-        'HOST': os.environ.get("SQL_HOST", "34.79.198.176"),
-        'PORT': os.environ.get("SQL_PORT", "5432"),
+        'HOST': os.environ.get("SQL_HOST"),
+        'PORT': os.environ.get("SQL_PORT"),
     }
 }
 
